fa051af56d5a57352d0d7dacd65c78a79422818f
Michael van Niekerk <mike@agri-io.co.za>
2023-07-30 21:59:19 +0200

    Add Pico PIO Example

---
+++ examples/pico-pio/src/main.rs
@@ -0,0 +1,207 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+#![feature(async_fn_in_trait)]
+
+pub mod pio_uart;
+
+extern crate alloc;
+
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
+
+use defmt::{error, info, unwrap};
+use embassy_executor::Spawner;
+use embassy_rp::bind_interrupts;
+use embassy_rp::peripherals::PIO0;
+
+use {defmt_rtt as _, panic_probe as _};
+
+use crate::pio_uart::uart_rx::{read_from_pio_uart_task, PioUartRx};
+use crate::pio_uart::uart_tx::PioUartTx;
+use crate::pio_uart::PioUart;
+use atat::AtatIngress;
+use atat::{asynch::Client, Buffers, Ingress};
+use embassy_rp::pio::InterruptHandler;
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_time::{Duration, Timer};
+use embedded_alloc::Heap;
+use seeed_lora_e5_at_commands::client::asynch::{JoinStatus, SeeedLoraE5Client};
+use seeed_lora_e5_at_commands::digester::LoraE5Digester;
+use seeed_lora_e5_at_commands::lora::types::{LoraClass, LoraJoinMode, LoraRegion};
+use seeed_lora_e5_at_commands::urc::URCMessages;
+
+const APP_KEY: u128 = 0xd65b042878144e038a744359c7cd1f9d;
+const DEV_EUI: u64 = 0x68419fa0f7e74b0d;
+
+// Chunk size in bytes when receiving data. Value should be matched to buffer
+// size of receive() calls.
+// TODO should be 1012
+const RX_SIZE: usize = 1012;
+
+// Constants derived from TX_SIZE and RX_SIZE
+const INGRESS_BUF_SIZE: usize = RX_SIZE;
+const URC_SUBSCRIBERS: usize = 0;
+// const URC_CAPACITY: usize = RX_SIZE * 1;
+const URC_CAPACITY: usize = 40;
+
+type AtIngress<'a> =
+    Ingress<'a, LoraE5Digester, URCMessages, INGRESS_BUF_SIZE, URC_CAPACITY, URC_SUBSCRIBERS>;
+
+type AtLoraE5Client<'a> = Client<'a, PioUartTx<'a>, INGRESS_BUF_SIZE>;
+
+bind_interrupts!(pub struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    static mut PIPE: embassy_sync::pipe::Pipe<ThreadModeRawMutex, 20> =
+        embassy_sync::pipe::Pipe::new();
+    let pio_uart = unsafe { PioUart::new(9600, p.PIO0, p.PIN_4, p.PIN_5, &mut PIPE) };
+    let (rx, tx, reader) = pio_uart.split();
+
+    // Atat client
+    let config = atat::Config::default()
+        .flush_timeout(Duration::from_millis(2000))
+        .cmd_cooldown(Duration::from_millis(200))
+        .tx_timeout(Duration::from_millis(2000));
+
+    let digester = LoraE5Digester::default();
+    static BUFFERS: Buffers<URCMessages, INGRESS_BUF_SIZE, URC_CAPACITY, URC_SUBSCRIBERS> =
+        atat::Buffers::<URCMessages, INGRESS_BUF_SIZE, URC_CAPACITY, URC_SUBSCRIBERS>::new();
+    let (ingress, client) = BUFFERS.split(tx, digester, config);
+    unwrap!(spawner.spawn(read_from_pio_uart_task(reader)));
+    unwrap!(spawner.spawn(read_task(ingress, rx)));
+    unwrap!(spawner.spawn(client_task(client)));
+}
+
+#[embassy_executor::task]
+async fn read_task(mut ingress: AtIngress<'static>, mut rx: PioUartRx<'static>) {
+    ingress.read_from(&mut rx).await;
+}
+
+#[embassy_executor::task]
+async fn client_task(client: AtLoraE5Client<'static>) {
+    let client = SeeedLoraE5Client::new(client).await;
+    if let Err(e) = client {
+        error!("Error creating client {}", e);
+        return;
+    }
+    let mut client = client.unwrap();
+
+    if let Err(e) = client.join_mode_set(LoraJoinMode::Otaa).await {
+        error!("Error setting join mode {}", e);
+    } else {
+        info!("Join mode set to OTAA");
+    }
+
+    if let Err(e) = client.dev_eui_set(DEV_EUI).await {
+        error!("Error setting dev eui {}", e);
+    } else {
+        info!("Dev EUI set");
+    }
+
+    if let Err(e) = client.app_eui_set(0x0).await {
+        error!("Error setting app eui {}", e);
+    } else {
+        info!("App EUI set");
+    }
+
+    if let Err(e) = client.app_key_set(APP_KEY).await {
+        error!("Error setting app key {}", e);
+    } else {
+        info!("App key set");
+    }
+
+    if let Err(e) = client.lora_region_set(LoraRegion::Eu868).await {
+        error!("Error setting lora region {}", e);
+    } else {
+        info!("Lora region set");
+    }
+
+    if let Err(e) = client.lora_class_set(LoraClass::ClassC).await {
+        error!("Error setting lora class {}", e);
+    } else {
+        info!("Lora class set to Class C");
+    }
+
+    if let Err(e) = client.adr_set(false).await {
+        error!("Error setting lora adr {}", e);
+    } else {
+        info!("Lora adr set to false");
+    }
+
+    if let Err(e) = client.dr_set(5).await {
+        error!("Error setting lora dr {}", e);
+    } else {
+        info!("Lora dr set to 5");
+    }
+
+    if let Err(e) = client.confirm_send_set(false).await {
+        error!("Error confirm set {}", e);
+    } else {
+        info!("Lora send ACK set to false");
+    }
+
+    if let Err(e) = client.auto_join_set(false, 3).await {
+        error!("Error setting auto join {}", e);
+    } else {
+        info!("Auto join disabled");
+    }
+
+    loop {
+        if matches!(
+            client.lora_join_otaa_and_wait_for_result().await,
+            Ok(JoinStatus::Success)
+        ) {
+            break;
+        }
+        error!("Failed to join, retrying");
+    }
+
+    let mut uplink_frame_count = 0;
+    let mut downlink_frame_count = 0;
+    loop {
+        let uplink_frame_count_get = client.uplink_frame_count().await;
+        if let Ok(uplink_frame_count_get) = uplink_frame_count_get {
+            if uplink_frame_count_get != uplink_frame_count {
+                info!("Uplink frame count: {:?}", uplink_frame_count_get);
+                uplink_frame_count = uplink_frame_count_get;
+            }
+        }
+        let _ = client.confirm_send_set(true).await;
+        match client.send(1, 12, b"Hello from Lora-E5").await {
+            Ok(_d) => {
+                info!("Sent bytes");
+            }
+            Err(e) => error!("Error sending {}", e),
+        }
+        for _i in 0..4 {
+            let downlink_frame_count_get =
+                client.downlink_message_count().await.unwrap_or_default();
+            if downlink_frame_count_get != downlink_frame_count {
+                info!(
+                    "Downlink frame count changed: {:?}",
+                    downlink_frame_count_get
+                );
+                downlink_frame_count = downlink_frame_count_get;
+                let rx = client.receive().await;
+                if rx.is_err() {
+                    error!("Error getting received bytes");
+                }
+                let (data, stats) = rx.unwrap();
+                let bytes = &data.payload;
+                info!(
+                    "Received bytes: {:?}, port: {:?}, RXWIN: {}, RSSI: {}, SNR: {}",
+                    data.length, data.port, stats.rxwin, stats.rssi, stats.snr
+                );
+
+                let l = core::str::from_utf8(&bytes[0..data.length]).unwrap();
+                info!("Bytes as string: {:?}", l);
+            }
+            Timer::after(Duration::from_secs(5)).await;
+        }
+    }
+}
---
+++ examples/pico-pio/src/pio_uart.rs
@@ -0,0 +1,236 @@
+use embassy_rp::peripherals::PIO0;
+use embassy_rp::pio::{Pio, PioPin};
+use embassy_rp::Peripheral;
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+
+use crate::pio_uart::uart_rx::{PioUartRx, PioUartRxReader};
+use crate::pio_uart::uart_tx::PioUartTx;
+use crate::Irqs;
+
+pub struct PioUart<'a> {
+    tx: PioUartTx<'a>,
+    rx: PioUartRx<'a>,
+    reader: PioUartRxReader<'a>,
+}
+
+impl<'a> PioUart<'a> {
+    pub fn new(
+        baud: u64,
+        pio: impl Peripheral<P = PIO0> + 'a,
+        tx_pin: impl PioPin,
+        rx_pin: impl PioPin,
+        pipe: &'a mut embassy_sync::pipe::Pipe<ThreadModeRawMutex, 20>,
+    ) -> PioUart<'a> {
+        let Pio {
+            mut common,
+            sm0,
+            sm1,
+            ..
+        } = Pio::new(pio, Irqs);
+        let reader = pipe.reader();
+        let writer = pipe.writer();
+
+        let (tx, origin) = PioUartTx::new(&mut common, sm0, tx_pin, baud, None);
+        let (rx, _, reader) =
+            PioUartRx::new(&mut common, sm1, rx_pin, baud, Some(origin), reader, writer);
+
+        PioUart { tx, rx, reader }
+    }
+
+    pub fn split(self) -> (PioUartRx<'a>, PioUartTx<'a>, PioUartRxReader<'a>) {
+        (self.rx, self.tx, self.reader)
+    }
+}
+
+pub mod uart_tx {
+    use core::convert::Infallible;
+
+    use embassy_rp::gpio::Level;
+    use embassy_rp::peripherals::PIO0;
+    use embassy_rp::pio::{
+        Common, Config, Direction, FifoJoin, PioPin, ShiftDirection, StateMachine,
+    };
+    use embassy_rp::relocate::RelocatedProgram;
+    use embedded_io::asynch::Write;
+    use embedded_io::Io;
+    use fixed::traits::ToFixed;
+    use fixed_macro::types::U56F8;
+
+    pub struct PioUartTx<'a> {
+        sm_tx: StateMachine<'a, PIO0, 0>,
+    }
+
+    impl<'a> PioUartTx<'a> {
+        pub fn new(
+            common: &mut Common<'a, PIO0>,
+            mut sm_tx: StateMachine<'a, PIO0, 0>,
+            tx_pin: impl PioPin,
+            baud: u64,
+            origin: Option<u8>,
+        ) -> (Self, u8) {
+            let mut prg = pio_proc::pio_asm!(
+                r#"
+                .side_set 1 opt
+
+                ; An 8n1 UART transmit program.
+                ; OUT pin 0 and side-set pin 0 are both mapped to UART TX pin.
+
+                    pull       side 1 [7]  ; Assert stop bit, or stall with line in idle state
+                    set x, 7   side 0 [7]  ; Preload bit counter, assert start bit for 8 clocks
+                bitloop:                   ; This loop will run 8 times (8n1 UART)
+                    out pins, 1            ; Shift 1 bit from OSR to the first OUT pin
+                    jmp x-- bitloop   [6]  ; Each loop iteration is 8 cycles.
+            "#
+            );
+            prg.program.origin = origin;
+            let tx_pin = common.make_pio_pin(tx_pin);
+            sm_tx.set_pins(Level::High, &[&tx_pin]);
+            sm_tx.set_pin_dirs(Direction::Out, &[&tx_pin]);
+
+            let relocated = RelocatedProgram::new(&prg.program);
+
+            let mut cfg = Config::default();
+
+            cfg.set_out_pins(&[&tx_pin]);
+            cfg.use_program(&common.load_program(&relocated), &[&tx_pin]);
+            cfg.shift_out.auto_fill = false;
+            cfg.shift_out.direction = ShiftDirection::Right;
+            cfg.shift_out.threshold = 32;
+            cfg.fifo_join = FifoJoin::TxOnly;
+            cfg.clock_divider = (U56F8!(125_000_000) / (8 * baud)).to_fixed();
+            sm_tx.set_config(&cfg);
+            sm_tx.set_enable(true);
+
+            // The 4 state machines of the PIO each have their own program counter that starts taking
+            // instructions at an offset (origin) of the 32 instruction "space" the PIO device has.
+            // It is up to the programmer to sort out where to place these instructions.
+            // From the pio_asm! macro you get a ProgramWithDefines which has a field .program.origin
+            // which takes an Option<u8>.
+            //
+            // When you load more than one RelocatedProgram into the PIO,
+            // you load your first program at origin = 0.
+            // The RelocatedProgram has .code().count() which returns a usize,
+            // for which you can then use as your next program's origin.
+            let offset = relocated.code().count() as u8 + origin.unwrap_or_default();
+            (Self { sm_tx }, offset)
+        }
+
+        pub async fn write_u8(&mut self, data: u8) {
+            self.sm_tx.tx().wait_push(data as u32).await;
+        }
+    }
+
+    impl Io for PioUartTx<'_> {
+        type Error = Infallible;
+    }
+
+    impl Write for PioUartTx<'_> {
+        async fn write(&mut self, buf: &[u8]) -> Result<usize, Infallible> {
+            for byte in buf {
+                self.write_u8(*byte).await;
+            }
+            Ok(buf.len())
+        }
+    }
+}
+
+pub mod uart_rx {
+    use core::convert::Infallible;
+
+    use embassy_rp::gpio::Level;
+    use embassy_rp::peripherals::PIO0;
+    use embassy_rp::pio::{
+        Common, Config, Direction, FifoJoin, PioPin, ShiftDirection, StateMachine,
+    };
+    use embassy_rp::relocate::RelocatedProgram;
+    use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+    use embedded_io::asynch::Read;
+    use embedded_io::Io;
+    use fixed::traits::ToFixed;
+    use fixed_macro::types::U56F8;
+
+    pub struct PioUartRx<'a> {
+        reader: embassy_sync::pipe::Reader<'a, ThreadModeRawMutex, 20>,
+    }
+
+    pub struct PioUartRxReader<'a> {
+        sm_rx: StateMachine<'a, PIO0, 1>,
+        writer: embassy_sync::pipe::Writer<'a, ThreadModeRawMutex, 20>,
+    }
+
+    impl<'a> PioUartRx<'a> {
+        pub fn new(
+            common: &mut Common<'a, PIO0>,
+            mut sm_rx: StateMachine<'a, PIO0, 1>,
+            rx_pin: impl PioPin,
+            baud: u64,
+            origin: Option<u8>,
+            reader: embassy_sync::pipe::Reader<'a, ThreadModeRawMutex, 20>,
+            writer: embassy_sync::pipe::Writer<'a, ThreadModeRawMutex, 20>,
+        ) -> (Self, u8, PioUartRxReader<'a>) {
+            let mut prg = pio_proc::pio_asm!(
+                r#"
+                ; Slightly more fleshed-out 8n1 UART receiver which handles framing errors and
+                ; break conditions more gracefully.
+                ; IN pin 0 and JMP pin are both mapped to the GPIO used as UART RX.
+
+                start:
+                    wait 0 pin 0        ; Stall until start bit is asserted
+                    set x, 7    [10]    ; Preload bit counter, then delay until halfway through
+                rx_bitloop:             ; the first data bit (12 cycles incl wait, set).
+                    in pins, 1          ; Shift data bit into ISR
+                    jmp x-- rx_bitloop [6] ; Loop 8 times, each loop iteration is 8 cycles
+                    jmp pin good_rx_stop   ; Check stop bit (should be high)
+
+                    irq 4 rel           ; Either a framing error or a break. Set a sticky flag,
+                    wait 1 pin 0        ; and wait for line to return to idle state.
+                    jmp start           ; Don't push data if we didn't see good framing.
+
+                good_rx_stop:           ; No delay before returning to start; a little slack is
+                    in null 24
+                    push                ; important in case the TX clock is slightly too fast.
+            "#
+            );
+            prg.program.origin = origin;
+            let relocated = RelocatedProgram::new(&prg.program);
+            let mut cfg = Config::default();
+            cfg.use_program(&common.load_program(&relocated), &[]);
+
+            let rx_pin = common.make_pio_pin(rx_pin);
+            sm_rx.set_pins(Level::High, &[&rx_pin]);
+            cfg.set_in_pins(&[&rx_pin]);
+            cfg.set_jmp_pin(&rx_pin);
+            sm_rx.set_pin_dirs(Direction::In, &[&rx_pin]);
+
+            cfg.clock_divider = (U56F8!(125_000_000) / (8 * baud)).to_fixed();
+            cfg.shift_in.auto_fill = false;
+            cfg.shift_in.direction = ShiftDirection::Right;
+            cfg.shift_in.threshold = 32;
+            cfg.fifo_join = FifoJoin::RxOnly;
+            sm_rx.set_config(&cfg);
+            sm_rx.set_enable(true);
+
+            let offset = relocated.code().count() as u8 + origin.unwrap_or_default();
+            (Self { reader }, offset, PioUartRxReader { sm_rx, writer })
+        }
+    }
+
+    impl Io for PioUartRx<'_> {
+        type Error = Infallible;
+    }
+
+    impl Read for PioUartRx<'_> {
+        async fn read(&mut self, buf: &mut [u8]) -> Result<usize, Infallible> {
+            let i = self.reader.read(buf).await;
+            Ok(i)
+        }
+    }
+
+    #[embassy_executor::task]
+    pub async fn read_from_pio_uart_task(mut pio_uart: PioUartRxReader<'static>) {
+        loop {
+            let byte = pio_uart.sm_rx.rx().wait_pull().await as u8;
+            pio_uart.writer.write(&[byte]).await;
+        }
+    }
+}
